feb39a1d235d4338d8127d08b9dd26b9383697aa
shujun <wsj20369@foxmail.com>
2020-11-04 10:13:22 +0800

    Revert "Remove needless (package-initialize)"
    After removed, Emacs always do the "Contacting ....." on each start,
    which causes the Emacs very slow. And I don't know why.
    
    This reverts commit 709cd0906c5131f8aa3307cf9f527a33392ddfd1.

--- emacs-configs/emacs-configs.el
+++ emacs-configs/emacs-configs.el
@@ -4,6 +4,7 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
 			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents)
   (package-install 'use-package))
