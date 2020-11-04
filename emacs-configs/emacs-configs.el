709cd0906c5131f8aa3307cf9f527a33392ddfd1
shujun <wsj20369@foxmail.com>
2020-11-04 09:39:20 +0800

    Remove needless (package-initialize)

--- emacs-configs/emacs-configs.el
+++ emacs-configs/emacs-configs.el
@@ -4,7 +4,6 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
 			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
-(package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents)
   (package-install 'use-package))
