1b38fa6c469833c9b50989bb1c3dd144746e1c46
shujun <wsj20369@foxmail.com>
2022-01-21 09:41:03 +0800

    Org mode: Update bullets chars

--- emacs-configs/orgmode-settings.el
+++ emacs-configs/orgmode-settings.el
@@ -14,7 +14,8 @@
   :ensure t
   :hook ('org-mode . 'org-bullets-mode)
   :custom
-  (org-bullets-bullet-list '("☯" "✿" "☢" "☀" "☭" "☛"))
+  (org-bullets-bullet-list '("☯" "◈" "❂" "▶" "❁" "☀" "⊙" "☛"))
+  ;; (org-bullets-bullet-list '("☯" "◈" "❂" "▶" "❁" "❀" "✿" "☢" "☀" "⊙" "☉" "☛"))
   )
 
 ;; 空闲时自动显示Agenda
@@ -28,11 +29,11 @@
 
 ;; 在线显示图片
 ;; C-c t 重新显示图片
-(org-toggle-inline-images t)
 (global-set-key (kbd "C-c t") 'org-redisplay-inline-images)
 
 ;; 设定图片的大小, 不要太大，也不要太小
 (setq org-image-actual-width '(600))
+(org-redisplay-inline-images)
 
 ;; 设定Tags的位置，不要太宽
 ;; (setq org-tags-column -68)
